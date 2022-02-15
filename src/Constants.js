e0cf8b436b161c676854784e078067b88ab6083f
Sujith Ramprasad <47356838+Sujithrt@users.noreply.github.com>
2022-02-15 16:55:42 +0530

    Delete src directory

--- src/Constants.js
+++
@@ -1,2 +0,0 @@
-export const API_URL = "http://localhost:9090"
-export const JPA_API_URL = "http://localhost:9090/jpa"
--- src/components/learning components/ThirdComponent.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function ThirdComponent() {
-    return (
-      <div className="thirdComponent">
-        Third Component
-      </div>
-    );
-  }
